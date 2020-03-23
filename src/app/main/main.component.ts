9113d9dff2a1f715d9ed475f6497dd2fb2dc7454
alshap <40171499+alshap@users.noreply.github.com>
2020-03-23 14:11:53 +0200

    RadSideDrawer nav added

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RadSideDrawer } from "nativescript-ui-sidedrawer";
+import * as app from "tns-core-modules/application";
+import { RouterExtensions } from "nativescript-angular/router";
 
 @Component({
   selector: 'ns-main',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  constructor(private routerExtensions: RouterExtensions) { }
 
   ngOnInit(): void {
+    this.closeDrawerOnInit()
   }
+
+  onDrawerButtonTap(): void {
+    const sideDrawer = <RadSideDrawer>app.getRootView();
+    sideDrawer.showDrawer();
+  }
+
+  closeDrawerOnInit(){
+    const sideDrawer = <RadSideDrawer>app.getRootView();
+    sideDrawer.closeDrawer();
+  }
+
 
 }
