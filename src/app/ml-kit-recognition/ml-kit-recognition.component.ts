5a8f6fa9e68dcc89166f01e1cce26deedf3d9023
alshap <40171499+alshap@users.noreply.github.com>
2020-03-25 16:57:23 +0200

    Added effect when detected an object

--- src/app/ml-kit-recognition/ml-kit-recognition.component.ts
+++ src/app/ml-kit-recognition/ml-kit-recognition.component.ts
@@ -12,6 +12,12 @@ export class MlKitRecognitionComponent extends AbstractMLKitViewComponent implem
 
   pickedImage: ImageSource;
   imagePath: ImageSource;
+  htmlString: String;
+  objectWidth: number;
+  objectHeight: number;
+  originX: number;
+  originY: number;
+  labelStyle: String;
 
   constructor() {
     super();
@@ -25,8 +31,18 @@ export class MlKitRecognitionComponent extends AbstractMLKitViewComponent implem
 
   onObjectDetectionResult(scanResult: any): void {
     const value: MLKitObjectDetectionResult = scanResult.value;
-    this.objs = value.objects;
-    value.objects.forEach(o => console.log(">> id: " + o.id + ", category: " + o.category + ", bounds: " + JSON.stringify(o.bounds)));
+    this.objs = value.objects; 
+    value.objects.forEach(o => {
+      this.objectWidth = o.bounds.size.width;
+      this.objectHeight = o.bounds.size.height;
+      this.originX = o.bounds.origin.x;
+      this.originY = o.bounds.origin.y;
+      console.log(">> id: " + o.id + ", category: " + o.category + ", bounds: " + JSON.stringify(o.bounds))
+    });
+    //this.htmlString = '<div style="background-color: transparent;width:'+this.objectWidth/10+'px'+';height:'+this.objectHeight/10+'px;border:5px solid;margin-left:'+this.originX+';margin-top:'+this.originY+';"></div>';
+    this.labelStyle = 'width:'+this.objectWidth+'px'+';height:'+this.objectHeight+
+                      'px;border-width: 8px;border-color: black;border-radius: 30;margin-left:'+
+                      (this.originX-this.objectWidth)+'px;margin-top:'+this.originY+'px';
   }
 
 }
